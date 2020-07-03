2182c62f42e79624165e4bb3286c74f43a7b8c1f
Mark Jones <mark.jones@torontopolice.on.ca>
2020-07-03 15:37:07 -0400

    Completed module

--- stats/data.py
+++ stats/data.py
@@ -5,7 +5,7 @@ import pandas as pd
 # create a list of the location of all the game files that end in 'eve' from the games directory
 game_files = glob.glob(os.path.join(os.getcwd(),'games','*.EVE'))
 
-list.sort(game_files)
+game_files.sort()
 
 #game_frames list to hold the generated dataframes from the for in loop below
 game_frames = []
@@ -17,16 +17,15 @@ for game_file in game_files:
 # this concats all the dataframes in the game_frames list into one dataframe
 games = pd.concat(game_frames, ignore_index=True)
 
-games['multi5'] = games['multi5'].replace('??','')
+games.loc[games['multi5'] == '??', ['multi5']] = ''
 
 identifiers = games['multi2'].str.extract(r'(.LS(\d{4})\d{5})')
-
-identifiers.fillna(method='ffill',inplace=True)
-identifiers.rename(columns={0:'game_id',1:'year'},inplace=True)
+identifiers = identifiers.fillna(method='ffill')
+identifiers.columns = ['game_id', 'year']
 
 games = pd.concat([games,identifiers],axis=1,sort=False)
 
-games.fillna('',inplace=True)
+games.fillna(' ',inplace=True)
 
 games.loc[:, 'type'] = pd.Categorical(games.loc[:, 'type'])
 
