016d551e6c57df2eedb5a36de993094a307755fa
Mark Jones <mark.jones@torontopolice.on.ca>
2020-07-14 17:10:07 -0400

    Completed module

--- stats/attendance.py
+++ stats/attendance.py
@@ -4,12 +4,13 @@ from stats.data import games
 
 attendance = games[['year','multi3']].loc[(games['type'] == 'info') & (games['multi2'] == 'attendance')]
 
-attendance = attendance.rename(columns={'multi3':'attendance'})
+attendance = attendance.rename(columns={'multi3':'Attendance','year':'Year'})
 
-attendance.loc[:,'attendance'] = pd.to_numeric(attendance.loc[:,'attendance'])
+attendance.loc[:,'Attendance'] = pd.to_numeric(attendance.loc[:,'Attendance'])
 
-attendance.plot(x='year', y='attendance', figsize=(15, 7),kind='bar')
-plt.xlabel(xlabel='year')
-plt.ylabel(ylabel='attendance')
-plt.axhline(attendance['attendance'].mean(),label='mean',linestyle='dashed',color='green')
+attendance.plot(x='Year', y='Attendance', figsize=(15, 7),kind='bar')
+plt.xlabel(xlabel='Year')
+plt.ylabel(ylabel='Attendance')
+#plt.axhline(attendance['attendance'].mean(),label='mean',linestyle='dashed',color='green')
+plt.axhline(y=attendance['Attendance'].mean(), label='Mean', linestyle='--', color='green')
 plt.show()
