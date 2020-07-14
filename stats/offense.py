621da18deab5ec179f2682697840a65f37b676cf
Mark Jones <mark.jones@torontopolice.on.ca>
2020-07-14 18:00:12 -0400

    Completed module

--- stats/offense.py
+++ stats/offense.py
@@ -8,7 +8,7 @@ plays.columns = ['type', 'inning', 'team', 'player', 'count', 'pitches', 'event'
 
 hits = plays.loc[plays['event'].str.contains('^(?:S(?!B)|D|T|HR)'),['inning','event']]
 
-hits['inning'] = hits.loc[:,['inning']].apply(pd.to_numeric)
+hits.loc[:, 'inning'] = pd.to_numeric(hits.loc[:, 'inning'])
 
 replacements = {r'^S(.*)': 'single',r'^D(.*)': 'double',r'^T(.*)': 'triple',r'^HR(.*)': 'hr'}
 
@@ -21,8 +21,7 @@ hits = hits.groupby(['inning','hit_type']).size().reset_index(name='count')
 
 hits['hit_type'] = pd.Categorical(hits['hit_type'],['single','double','triple','hr'])
 
-hits = hits.sort_values(by=['inning','hit_type'])
-
+hits = hits.sort_values(['inning', 'hit_type'])
 hits = hits.pivot(index='inning',columns='hit_type',values='count')
 
 hits.plot.bar(stacked=True)
