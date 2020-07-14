d1b4194a2f69aa540d9964418c9768ca6baf1999
Mark Jones <mark.jones@torontopolice.on.ca>
2020-07-14 17:14:51 -0400

    Completed module

--- stats/attendance.py
+++ stats/attendance.py
@@ -12,8 +12,8 @@ attendance.columns = ['year', 'attendance']
 attendance.loc[:,'attendance'] = pd.to_numeric(attendance.loc[:,'attendance'])
 
 attendance.plot(x='year', y='attendance', figsize=(15, 7),kind='bar')
-plt.xlabel(xlabel='Year')
-plt.ylabel(ylabel='Attendance')
+plt.xlabel('Year')
+plt.ylabel('Attendance')
 #plt.axhline(attendance['attendance'].mean(),label='mean',linestyle='dashed',color='green')
 plt.axhline(y=attendance['attendance'].mean(), label='Mean', linestyle='--', color='green')
 plt.show()
